eb084208d78700c82371e6c1189f2027dfb58c1e
murimichege <chegeeddie@gmail.com>
2022-09-19 16:27:13 +0300

    https

--- admin-panel/src/api/axiosClient.js
+++ admin-panel/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import queryString from 'query-string'
 
-const baseUrl = 'https://localhost/:3000/api/'
+const baseUrl = 'http://localhost/:3000/api/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
