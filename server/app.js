778fdd725d5382d8d10a35ef407c7577d6a808ad
murimichege <chegeeddie@gmail.com>
2022-09-19 17:09:26 +0300

    CORS config

--- server/app.js
+++ server/app.js
@@ -6,7 +6,15 @@ const cors = require('cors');
 const appRoute = require('./src/routes');
 
 const app = express();
-
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    next();
+  });
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,16 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', appRoute);
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
-    next();
-  });
+
 
 module.exports = app;
