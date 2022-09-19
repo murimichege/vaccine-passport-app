93133efe9f61b53c5f6b416d1cd024a4c23d6d76
murimichege <chegeeddie@gmail.com>
2022-09-19 13:54:04 +0300

    changed to the correct base url

--- admin-panel/src/api/axiosClient.js
+++ admin-panel/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import queryString from 'query-string'
 
-const baseUrl = 'https://vaccine-passport-backend.vercel.app/'
+const baseUrl = 'https://vaccine-passport-app-backend.vercel.app/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
