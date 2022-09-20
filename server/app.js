3c9f5e3fd792919535486e1c2c46ffa0fd2b730b
murimichege <chegeeddie@gmail.com>
2022-09-20 11:55:52 +0300

    set cors headers

--- server/app.js
+++ server/app.js
@@ -11,16 +11,11 @@ const appRoute = require('./src/routes');
 const app = express();
 app.use(cors({ origin: "https://vaccine-passport-app.vercel.app/", credentials: true }))
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', 'https://vaccine-passport-app.vercel.app/')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    next();
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
   });
 
 // console.log("+++++++++++success" )
